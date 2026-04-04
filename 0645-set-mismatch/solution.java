0d106fcdb75e8aa4755f946d457231e379a4af09
Chaitanya Kshirsagar <cmkshirsagar65@gmail.com>
2026-04-04 13:50:24 +0000

    Sync LeetCode submission Runtime - 2 ms (95.12%), Memory - 47.9 MB (25.62%)

---
+++ 0645-set-mismatch/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int freq[] = new int[n + 1];
+
+        for(int num : nums){
+            freq[num]++;
+        }
+        int duplicate = -1;int missing = -1;
+
+        for(int i=1;i<=n;i++){
+            if(freq[i] == 0) missing = i;
+            if(freq[i] == 2) duplicate = i;
+        }
+
+        return new int []{duplicate,missing};
+    }
+}
